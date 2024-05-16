fddda3656f9a4f376ec7f29911549830bdbd1494
Muhammad Akasha <153507162+muhammad-akasha@users.noreply.github.com>
2024-05-16 23:50:15 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,93 +1,127 @@
-//  function isElementInViewport(el) {
-//         var rect = el.getBoundingClientRect();
-//         return (
-//             rect.top >= -100 &&
-//             rect.left >= 0 &&
-//             rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-//             rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-//         );
-//     }
+let iconsCon = document.querySelector(".icon-container");
+let childIcons = iconsCon.children;
 
-//     function animateOnScroll() {
-//         var elements = document.querySelectorAll('.animate-on-scroll');
-//         elements.forEach(function(el) {
-//             if (isElementInViewport(el)) {
-//                 var animation = el.getAttribute('data-caption-animate');
-//                 el.classList.add('animate__animated', 'animate__' + animation);
-//             }
-//         });
+// iconsCon.addEventListener("click", () => {
+//   for (let i = 0; i < childIcons.length; i++) {
+//     if (iconsCon[i].contains.classList("line-1", "line-2", "line-3")) {
+//       childIcons[i].classList.remove("line-1", "line-2", "line-3");
+//       childIcons[0].classList.add("rotate-line-1");
+//       childIcons[1].classList.add("line-2");
+//       childIcons[2].classList.add("rotate-line-3");
+//       iconsCon.style.transform = "rotate(180deg)";
+//     } else {
+//       iconsCon.style.transform = "rotate(0deg)";
+//       iconsCon[i].classList.add("line-1", "line-2", "line-3");
 //     }
+//   }
+// });
 
-//     window.addEventListener('scroll', animateOnScroll);
-//     animateOnScroll(); // Trigger on page load 
+function clasAdd() {
+  iconsCon.style.transform = "rotate(180deg)";
+  for (let i = 0; i < childIcons.length; i++) {
+    childIcons[i].classList.remove("line-1", "line-2", "line-3");
+    childIcons[0].classList.add("rotate-line-1");
+    childIcons[1].classList.add("line-2");
+    childIcons[2].classList.add("rotate-line-3");
+  }
+}
+
+// adding hover effect on image
+
+let image1;
+let image2;
+function hoverImg() {
+  image1 = document.getElementById("image1");
+  image2 = document.getElementById("image2");
+  image1.classList.remove("one");
+  image2.classList.remove("two");
+  image1.classList.add("img-hover-1");
+  image2.classList.add("img-hover-2");
+}
+
+// adding remove hover function when mouseout
+
+function removeHover() {
+  image1.classList.add("one");
+  image2.classList.add("two");
+  image1.classList.remove("img-hover-1");
+  image2.classList.remove("img-hover-2");
+}
+
+// on reload scroll back to top
 
 function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 }
+
+// fixed the navbar on scroll down
+
 let navbar = document.getElementById("navbar");
 
 window.addEventListener("scroll", function() {
-    let scroll = window.scrollY;
+  let scroll = window.scrollY;
 
-    if (scroll >= 100){
-        navbar.classList.add("fixed-nav");
-    } else {
-        navbar.classList.remove("fixed-nav");
-    }
+  if (scroll >= 100) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
 });
 
+// adding animation effects one by one while user scrolling .
 
+window.addEventListener("load", scrollToTop);
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  var offset = -30; // Additional offset from the top
+  return (
+    rect.top + offset <= window.innerHeight &&
+    rect.bottom >= 0 &&
+    rect.left >= 0 &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
 
-window.addEventListener('load', scrollToTop);
-    function isElementInViewport(el) {
-        var rect = el.getBoundingClientRect();
-        var offset = -30; // Additional offset from the top
-        return (
-            rect.top + offset <= window.innerHeight &&
-            rect.bottom >= 0 &&
-            rect.left >= 0 &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
+function animateOnScroll() {
+  var elements = document.querySelectorAll(".animate-on-scroll");
+  elements.forEach(function(el) {
+    if (isElementInViewport(el)) {
+      var animation = el.getAttribute("data-caption-animate");
+      el.classList.add("animate__animated", "animate__" + animation);
     }
+  });
+}
 
-    function animateOnScroll() {
-        var elements = document.querySelectorAll('.animate-on-scroll');
-        elements.forEach(function(el) {
-            if (isElementInViewport(el)) {
-                var animation = el.getAttribute('data-caption-animate');
-                el.classList.add('animate__animated', 'animate__' + animation);
-            }
-        });
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this,
+      args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+// add animation effect with using class names and with data caption attribute.
+
+function checkSlide() {
+  var elements = document.querySelectorAll(".animate-on-scroll");
+  elements.forEach(function(el) {
+    if (isElementInViewport(el)) {
+      var animation = el.getAttribute("data-caption-animate");
+      el.classList.add("animate__animated", "animate__" + animation);
     }
+  });
+}
 
-    function debounce(func, wait = 20, immediate = true) {
-        var timeout;
-        return function() {
-            var context = this, args = arguments;
-            var later = function() {
-                timeout = null;
-                if (!immediate) func.apply(context, args);
-            };
-            var callNow = immediate && !timeout;
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-            if (callNow) func.apply(context, args);
-        };
-    }
-
-    function checkSlide() {
-        var elements = document.querySelectorAll('.animate-on-scroll');
-        elements.forEach(function(el) {
-            if (isElementInViewport(el)) {
-                var animation = el.getAttribute('data-caption-animate');
-                el.classList.add('animate__animated', 'animate__' + animation);
-            }
-        });
-    }
-
-    window.addEventListener('scroll', debounce(checkSlide));
-    checkSlide(); // Trigger on page load
-
+window.addEventListener("scroll", debounce(checkSlide));
+checkSlide(); // Trigger on page load
